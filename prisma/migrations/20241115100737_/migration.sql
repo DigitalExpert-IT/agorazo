d36ff3aec832f819497a1a6dd336ad614656a068
Y0SH4 <yosa.agung13@gmail.com>
2024-11-15 17:21:32 +0700

    migration

--- prisma/migrations/20241115100737_/migration.sql
+++ prisma/migrations/20241115100737_/migration.sql
@@ -13,6 +13,17 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "PendingUser" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "verifyToken" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "userId" TEXT NOT NULL,
@@ -77,6 +88,12 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_verifyToken_key" ON "User"("verifyToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PendingUser_email_key" ON "PendingUser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PendingUser_verifyToken_key" ON "PendingUser"("verifyToken");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
